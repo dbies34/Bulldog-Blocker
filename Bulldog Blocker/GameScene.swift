dab9686f5e70ea0df42add1fa98f884b25819221
Drew Bies <ddbies@hotmail.com>
2020-11-28 23:35:53 -0800

    added ball array and animation

--- Bulldog Blocker/GameScene.swift
+++ Bulldog Blocker/GameScene.swift
@@ -10,30 +10,66 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    var ballArray: [SKSpriteNode] = [SKSpriteNode]()
+    var activeBalls: [SKSpriteNode] = [SKSpriteNode]()
+    var inactiveBalls: [SKSpriteNode] = [SKSpriteNode]()
     var hoop = SKSpriteNode()
     
     func setup(){
         print("setup")
         hoop = self.childNode(withName: "hoop") as! SKSpriteNode
-        // fill the ball array
-        for _ in 0...10 {
-            var ball = SKSpriteNode(imageNamed: "basketball")
-            ball.size.height = 50.0
-            ball.size.width = 50.0
-            ball.position.x = 10.0
-            ball.position.y = 10.0
-            
-            ballArray.append(ball)
-        }
+ 
+        
     }
     
+    // runs when the scene is displayed
     override func didMove(to view: SKView) {
         self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
         setup()
-       
+        addBall()
+        // game loop
+        run(SKAction.repeatForever(
+            SKAction.sequence([
+                SKAction.run(moveBalls),
+                //SKAction.run(addBall),
+                SKAction.run(moveEnemies),
+                                SKAction.wait(forDuration: 0.01)])))
     }
     
+    // add a new ball to the scene
+    func addBall(){
+        // if there is no ball to reuse, make a new one
+        if inactiveBalls.count == 0{
+            let ball = SKSpriteNode(imageNamed: "basketball")
+            ball.size.height = 50.0
+            ball.size.width = 50.0
+            ball.position.x = 0.0
+            ball.position.y = -260.0
+            activeBalls.append(ball)
+            addChild(ball)
+        } else{
+            // reuse the balls from the inactive ball array
+            if let ball = inactiveBalls.popLast(){
+                ball.position.x = 0.0
+                ball.position.y = -240.0
+            }
+            
+        }
+        
+        
+        
+    }
+    
+    // move the balls and check for collisions
+    func moveBalls(){
+        for ball in activeBalls{
+            ball.position.y += 1.0
+        }
+    }
+    
+    // move the enemies towards the center and check for collisions
+    func moveEnemies(){
+        
+    }
     
     func touchDown(atPoint pos : CGPoint) {
         print("touch down")
